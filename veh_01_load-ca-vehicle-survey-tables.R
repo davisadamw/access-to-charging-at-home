a2e19c0cda2cb0b91a057f8ce7b5aaf47888b617
Adam Davis <davisadamw@gmail.com>
2020-03-04 17:01:55 -0800

    updated to work with 2019 vehicle survey

--- veh_01_load-ca-vehicle-survey-tables.R
+++ veh_01_load-ca-vehicle-survey-tables.R
@@ -2,23 +2,48 @@ library(tidyverse)
 library(definer)
 
 # load definitions
-veh_survey_definitions <- read_rds('Data/ca_veh_survey_defs.rds')
+veh_survey_definitions <- read_rds('Data/ca_veh_survey_defs_2019.rds')
 
 veh_survey_dict <- veh_survey_definitions %>% 
   def_prep(var_name, value, value_label)
 
 # load the survey files we care about and apply the definitions
-ca_veh_survey <- list.files('Data/ca_veh_surv_res', 'survey_res*',
+# ... some of the columns have weird values lower down the rows, so guess/sniff based on all rows
+ca_veh_survey <- list.files('Data/tsdc-2019-california-vehicle-survey/data',
+                            'survey_res*',
                             recursive = TRUE, full.names = TRUE) %>% 
   set_names(str_extract(., '[a-z]+(?=\\.csv)')) %>% 
-  map(read_csv) %>% 
+  map(read_csv, guess_max = 40000) %>% 
   map(def_recode_all, def_list = veh_survey_dict, .default = 'unspecified')
 
 
-# parking info
+# grab a little info about vehicles 
+vehicles_fewvars <- ca_veh_survey$vehicle %>% 
+  select(sampno:vehicle_num, year, make_clean, fuel_clean, 
+         veh_type_clean, flag_hev:flag_bev, annual_mileage, mpg,
+         current_primary_driver, primary_driver_id) %>% 
+  # need to get the ev category variable down into one column
+  #mutate(ev_cat = )
+
+# parking info is a multi-option thing stored in the definitions file
+parking_descs <- veh_survey_definitions %>% 
+  filter(str_starts(var_name, 'home_parking')) %>% 
+  distinct(var_name, var_label) %>% 
+  mutate(parking_type = str_extract(var_label, '(?<=\\?: )[A-Za-z ]+') %>% 
+           snakecase::to_snake_case()) %>% 
+  select(var_name, parking_type) %>% 
+  deframe()
+
+rename_parking <- function(name) parking_descs[name]
+
+# parking info ... multi choice stored awkwardly, need to rename variables
+# ... also attach vehicle info and drop households without any vehicles
 parking_with_vehs <- ca_veh_survey$main %>% 
-  select(sampno, county, housing, parkingtype, parkingpay) %>% 
-  left_join(select(ca_veh_survey$currentvehs, sampno:vehmake, vehfueltype), by = 'sampno')
+  select(sampno, source, california, region, county, 
+         home_electricity_access, 
+         starts_with('home_parking'), -home_parking_8_x) %>% 
+  rename_at(vars(starts_with('home_parking')), rename_parking) %>% 
+  inner_join(vehicles_fewvars, by = 'sampno')
 
 # basic parking info
 parking_with_vehs <- parking_with_vehs %>% 
