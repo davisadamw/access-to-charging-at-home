5994e278c243cc3fe6446b99e12d1513766e1646
Adam Davis <davisadamw@gmail.com>
2020-03-04 21:37:07 -0800

    parking X vehicle analysis

--- veh_01_load-ca-vehicle-survey-tables.R
+++ veh_01_load-ca-vehicle-survey-tables.R
@@ -16,14 +16,26 @@ ca_veh_survey <- list.files('Data/tsdc-2019-california-vehicle-survey/data',
   map(read_csv, guess_max = 40000) %>% 
   map(def_recode_all, def_list = veh_survey_dict, .default = 'unspecified')
 
+# what percent of households have 2 commutes >= 30 miles one way?
+ca_veh_survey$person %>% 
+  filter(work_mode == 'Drive alone using household vehicle') %>% 
+  mutate(commute_30plus = work_distance >= 30) %>% 
+  group_by(sampno) %>% 
+  summarise(n_tot = n(),
+            n_30  = sum(commute_30plus)) %>% 
+  count(n_30 > 1)
 
 # grab a little info about vehicles 
 vehicles_fewvars <- ca_veh_survey$vehicle %>% 
-  select(sampno:vehicle_num, year, make_clean, fuel_clean, 
-         veh_type_clean, flag_hev:flag_bev, annual_mileage, mpg,
-         current_primary_driver, primary_driver_id) %>% 
   # need to get the ev category variable down into one column
-  #mutate(ev_cat = )
+  mutate(ev_cat = case_when(flag_hev  == 'Selected' ~ 'Hybrid',
+                            flag_phev == 'Selected' ~ 'PHEV',
+                            flag_fcev == 'Selected' ~ 'FCEV',
+                            flag_bev  == 'Selected' ~ 'BEV',
+                            TRUE      ~ 'ICEV')) %>% 
+  select(sampno:vehicle_num, year, make_clean, fuel_clean, 
+         veh_type_clean, ev_cat, annual_mileage, mpg,
+         current_primary_driver, primary_driver_id) 
 
 # parking info is a multi-option thing stored in the definitions file
 parking_descs <- veh_survey_definitions %>% 
@@ -36,29 +48,44 @@ parking_descs <- veh_survey_definitions %>%
 
 rename_parking <- function(name) parking_descs[name]
 
+parking_ranking <- tribble(
+  ~parking_type,                             ~parking_level,    ~parking_ranking,
+  'driveway',                                'driveway',        4,
+  'on_the_street',                           'street',          9,
+  'attached_garage',                         'garage_attached', 1,
+  'detached_garage',                         'garage_detached', 2,
+  'unassigned_parking_in_lot_or_garage',     'lot',             3,
+  'none_of_these_are_available_at_my_home',  'street',          9,
+  'assigned_parking_in_lot_or_garage',       'lot',             4,
+  'carport',                                 'carport',         3,
+  'other',                                   'street',          9
+)
+
+# only keep the "best" parking situation per household
+parking_status <- ca_veh_survey$main %>% 
+  select(sampno, starts_with('home_parking'), -home_parking_8_x) %>% 
+  pivot_longer(-sampno, names_to = 'var_name', values_to = 'present') %>% 
+  filter(present == 'Selected') %>% 
+  left_join(select(veh_survey_definitions, var_name, var_label), 
+            by = 'var_name') %>% 
+  mutate(parking_type = str_extract(var_label, '(?<=\\?: )[A-Za-z ]+') %>% 
+           snakecase::to_snake_case()) %>% 
+  left_join(parking_ranking, by = 'parking_type') %>% 
+  group_by(sampno) %>% 
+  filter(parking_ranking == min(parking_ranking)) %>% 
+  ungroup() %>% 
+  distinct(sampno, parking_level)
+  
+    
+
 # parking info ... multi choice stored awkwardly, need to rename variables
 # ... also attach vehicle info and drop households without any vehicles
 parking_with_vehs <- ca_veh_survey$main %>% 
-  select(sampno, source, california, region, county, 
-         home_electricity_access, 
-         starts_with('home_parking'), -home_parking_8_x) %>% 
-  rename_at(vars(starts_with('home_parking')), rename_parking) %>% 
+  select(sampno, source, region,
+         home_electricity_access) %>% 
+  left_join(parking_status, by = 'sampno') %>% 
   inner_join(vehicles_fewvars, by = 'sampno')
 
-# basic parking info
-parking_with_vehs <- parking_with_vehs %>% 
-  mutate(charging_case = 
-           case_when(str_starts(housing, 'Single family house not attached') &
-                       parkingtype == 'Personal garage' ~ 'highest',
-                     str_starts(housing, 'Single family house') &
-                       str_starts(parkingtype, 'Personal') ~ 'high',
-                     TRUE ~ 'low'))
-
 parking_with_vehs %>% 
-  count(vehtype, vehfueltype, charging_case) %>% 
-  pivot_wider(names_from = charging_case, values_from = n, values_fill = list(n = 0)) %>% 
-  mutate(total = rowSums(.[, c('high', 'highest', 'low')])) %>% 
-  mutate_at(vars(high, highest, low), ~ . / total) %>% 
-  View()
-
+  write_rds('Data/parking_summary_2019.rds')
 
